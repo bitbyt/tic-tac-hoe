89f978d0cd75fca9c4c7137b3145c746131b81ce
Luqman Mohamed <lukas.woodman@gmail.com>
2016-07-04 21:03:23 +0800

    game finally runs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,111 +1,172 @@
 //I miss JQuery
+function init() {
+  console.log("Tic Tac Toe Ready.");
+  var currentPlayer = 'Player 1';
+  //create array to store game logic. Fill array with box IDs.
+  var boardCount = 0;
 
-//create array to store game logic. Fill array with box IDs.
-var gameArray = [];
-var gameBoard = document.getElementsByClassName('box');
-var gameStatus = document.querySelector('h4');
-var resetButton = document.getElementById('ctrl-btn');
+  var board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
 
-function hasClass(el, className) {
-  if (el.classList)
-    return el.classList.contains(className)
-  else
-    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
-}
+  var gameBoard = document.getElementsByClassName('box');
+  var gameStatus = document.querySelector('h4');
+  var resetButton = document.getElementById('ctrl-btn');
 
-function addClass(el, className) {
-  if (el.classList)
-    el.classList.add(className)
-  else if (!hasClass(el, className)) el.className += " " + className
-}
-
-function removeClass(el, className) {
-  if (el.classList)
-    el.classList.remove(className)
-  else if (hasClass(el, className)) {
-    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-    el.className=el.className.replace(reg, ' ')
-  }
-}
-
-var winningCombinations = [
-  // Horizontals
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-
-  // Verticals d
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-
-  // Diagonals
-  [0, 4, 8],
-  [2, 4, 6]
-];
-
-//create player classes
-var player1 = 'Player One', player2 = 'Player Two';
-var addCross = '<div class="content"><i class="crossIcon" aria-hidden="fa"></i></div>';
-var addCircle = '<div class="content"><i class="circleIcon"></i></div>';
-
-//check for turn
-var playerTurn = true;
-
-//start game with score of zero
-
-//function to check for win
-/* function checkWin(playerClass) {
-  //id selector function
-  function thisId(theId) {
-    return document.getElementById('\'' + theId + '\'');
-  }
-  //define the 8 rules to win
- if ( thisId(1).classList.contains(playerClass) && thisId(2).classList.contains(playerClass) && thisId(3).classList.contains(playerClass) || thisId(4).classList.contains(playerClass) && thisId(5).classList.contains(playerClass) && thisId(6).classList.contains(playerClass) || thisId(7).classList.contains(playerClass) && thisId(8).classList.contains(playerClass) && thisId(9).classList.contains(playerClass) || thisId(1).classList.contains(playerClass) && thisId(5).classList.contains(playerClass) && thisId(9).classList.contains(playerClass) || thisId(3).classList.contains(playerClass) && thisId(5).classList.contains(playerClass) && thisId(7).classList.contains(playerClass) || thisId(1).classList.contains(playerClass) && thisId(4).classList.contains(playerClass) && thisId(7).classList.contains(playerClass) || thisId(2).classList.contains(playerClass) && thisId(5).classList.contains(playerClass) && thisId(8).classList.contains(playerClass) || thisId(3).classList.contains(playerClass) && thisId(6).classList.contains(playerClass) && thisId(9).classList.contains(playerClass)) {
-    console.log(player + " has won");
-    return true;
-  } else {
-    return false;
-  }
-} */
-//check if all boxes are full which ends in a tie
-
-//score counter
-
-//clear board
-
-resetButton.addEventListener('click', resetBoard);
-
-function resetBoard() {
-  for ( var i = 0; i < gameBoard.length; i++ ) {
-    gameBoard[i].innerHTML = "";
-    removeClass(gameBoard[i], 'circle');
-    removeClass(gameBoard[i], 'cross');
-    gameStatus.innerHTML = "A simple game of tic tac toe";
-  }
-}
-
-//basic ai
-
-//start game for human v human
+  var addCross = '<div id="playerOne" class="content"><i class="crossIcon" aria-hidden="fa"></i></div>';
+  var addCircle = '<div id="playerTwo" class="content"><i class="circleIcon"></i></div>';
 
   for ( var i = 0; i < gameBoard.length; i++ ) {
     gameBoard[i].addEventListener('click', playerMove);
   }
 
-  function playerMove() {
-    if (this.innerHTML === "" && playerTurn) {
-      this.classList.add("cross");
-      this.innerHTML = addCross;
-      gameStatus.innerHTML = "Player 2, it's your turn!";
-      playerTurn = false;
-    } else if (this.innerHTML === "" && !playerTurn) {
-      this.classList.add("circle");
-      this.innerHTML = addCircle;
-      gameStatus.innerHTML = "Player 1, your move!";
-      playerTurn = true;
+  function removeClass(el, className) {
+    if (el.classList)
+      el.classList.remove(className);
+    else if (hasClass(el, className)) {
+      var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+      el.className=el.className.replace(reg, ' ');
+    }
+  }
+  resetButton.addEventListener('click', resetBoard);
+
+  function resetBoard() {
+    for ( var i = 0; i < gameBoard.length; i++ ) {
+      gameBoard[i].innerHTML = "";
+      removeClass(gameBoard[i], 'circle');
+      removeClass(gameBoard[i], 'cross');
+      gameStatus.innerHTML = "A simple game of tic tac toe";
     }
   }
 
+  function playerMove() {
+    var selectedBox = event.target;
+    var unmarkedBox = this.innerHTML === "";
 
-//start game for ai v human
+    if (currentPlayer === 'Player 1' && unmarkedBox) {
+      this.classList.add('cross');
+      this.innerHTML = addCross;
+      setMove('X', this.id);
+      checkForWinner();
+      switchPlayer('Player 2');
+    } else if (currentPlayer === 'Player 2' && unmarkedBox) {
+      this.classList.add('circle');
+      this.innerHTML = addCircle;
+      setMove('O', this.id);
+      checkForWinner();
+      switchPlayer('Player 1');
+    } else {
+        alert('Whoops, this box has been marked.');
+    }
+  }
+
+  function switchPlayer(player) {
+      //Toggle value of current player
+      currentPlayer = player;
+      //Update display display to show which player is next
+      gameStatus.innerHTML = "It is " + player + "'s turn.";
+
+  }
+
+  function setMove(move, buttonId) {
+        if (buttonId === 'b1') {
+            board[0][0] = move;
+        } else if (buttonId === 'b2') {
+            board[0][1] = move;
+        } else if (buttonId === 'b3') {
+            board[0][2] = move;
+        } else if (buttonId === 'b4') {
+            board[1][0] = move;
+        } else if (buttonId === 'b5') {
+            board[1][1] = move;
+        } else if (buttonId === 'b6') {
+            board[1][2] = move;
+        } else if (buttonId === 'b7') {
+            board[2][0] = move;
+        } else if (buttonId === 'b8') {
+            board[2][1] = move;
+        } else {
+            //Set button b9
+            board[2][2] = move;
+        }
+        boardCount++;
+    }
+
+    function getButtonState(buttonId) {
+        if (buttonId === 'b1') {
+            return board[0][0];
+        } else if (buttonId === 'b2') {
+            return board[0][1];
+        } else if (buttonId === 'b3') {
+            return board[0][2];
+        } else if (buttonId === 'b4') {
+            return board[1][0];
+        } else if (buttonId === 'b5') {
+            return board[1][1];
+        } else if (buttonId === 'b6') {
+            return board[1][2];
+        } else if (buttonId === 'b7') {
+            return board[2][0];
+        } else if (buttonId === 'b8') {
+            return board[2][1];
+        } else {
+            //return val of button b9.
+            return board[2][2];
+        }
+    }
+
+    function alertWinner(buttonValue){
+        if(buttonValue === 'O'){
+            alert('Game Over, Player 2 wins!');
+            gameStatus.innerHTML = "Player 2 wins!";
+            resetBoard();
+        }else {
+            alert('Game Over, Player 1 wins!');
+            gameStatus.innerHTML = "Player 1 wins!";
+            resetBoard();
+        }
+    }
+
+    function checkForWinner() {
+        //Check row 1 win
+        if ((board[0][0] === board[0][1]) && (board[0][1] === board[0][2]) && (board[0][2] !== null)) {
+            alertWinner(board[0][0]);
+        }
+        else if ((board[1][0] === board[1][1]) && (board[1][1] === board[1][2]) && (board[1][2] !== null)) {
+            alertWinner(board[1][0]);
+        }
+        else if ((board[2][0] === board[2][1]) && (board[2][1] === board[2][2]) && (board[2][2] !== null)) {
+            alertWinner(board[2][0]);
+        }
+        else if ((board[0][0] === board[1][0]) && (board[1][0] === board[2][0]) && (board[2][0] !== null)) {
+            alertWinner(board[0][0]);
+        }
+        else if ((board[0][1] === board[1][1]) && (board[1][1] === board[2][1]) && (board[2][1] !== null)) {
+            alertWinner(board[0][1]);
+        }
+        else if ((board[0][2] === board[1][2]) && (board[1][2] === board[2][2]) && (board[2][2] !== null)) {
+            alertWinner(board[0][2]);
+        }
+        else if ((board[0][0] === board[1][1]) && (board[1][1] === board[2][2]) && (board[2][2] !== null)) {
+            alertWinner(board[0][0]);
+        }
+        else if ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0]) && (board[2][0] !== null)) {
+            alertWinner(board[0][2]);
+        }
+        else if(boardCount === 9){
+            //boardCount stores the number of spaces already filled in the board
+            //If value is == 9 and not a winning combination, it is a tie.
+            alert('It is a tie!');
+            resetBoard();
+        }else {
+            console.log('No winner yet!');
+        }
+    }
+
+
+  //start game for ai v human
+}
+
+window.addEventListener("load",init,false);
